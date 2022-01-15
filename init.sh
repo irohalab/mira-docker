8048f0f704700a48f9d91b2d15d8e1153749320a
Everett Summer <55684557+EverettSummer@users.noreply.github.com>
2022-01-15 03:27:24 -0800

    add pip install wheel to fix pip issue

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ BASEDIR=$(dirname $0)
 python3 -m venv ${BASEDIR}/venv
 
 source ${BASEDIR}/venv/bin/activate
-
+python3 -m pip install wheel
 python3 -m pip install -r requirements.txt
 
 python3 ./init_helper.py
