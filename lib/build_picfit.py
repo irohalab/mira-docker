6536383036ac7fcc0b751fc6fa96c64b13b4769b
Everett Summer <55684557+EverettSummer@users.noreply.github.com>
2022-01-21 20:30:32 -0800

    fix bug in docker-compose, update build_picfit.py

--- lib/build_picfit.py
+++ lib/build_picfit.py
@@ -7,16 +7,16 @@ from colored import attr, fg
 
 
 def build_picfit():
-    tmp_folder = './picfit/build'
-    if exists(tmp_folder):
-        print(fg(111) + 'clean tmp folder' + attr('reset'))
-        rmtree(tmp_folder)
-    mkdir(tmp_folder)
+    # tmp_folder = './picfit/build'
+    # if exists(tmp_folder):
+    #     print(fg(111) + 'clean tmp folder' + attr('reset'))
+    #     rmtree(tmp_folder)
+    # mkdir(tmp_folder)
+    #
+    # return_code = subprocess.call('git clone https://github.com/thoas/picfit.git picfit', cwd=tmp_folder, shell=True)
+    # if return_code != 0:
+    #     raise Exception('failed to clone picfit repo')
 
-    return_code = subprocess.call('git clone https://github.com/thoas/picfit.git picfit', cwd=tmp_folder, shell=True)
-    if return_code != 0:
-        raise Exception('failed to clone picfit repo')
-
-    return_code = subprocess.call('docker build -t picfit-local .', cwd=join(tmp_folder, 'picfit'), shell=True)
+    return_code = subprocess.call('docker build -t mira/picfit https://github.com/thoas/picfit.git', cwd='./picfit', shell=True)
     if return_code != 0:
         raise Exception('failed to build picfit')
